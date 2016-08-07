5388e68b07f5be57929d0081e029764c2ec85b4f
Samir Jafferali <samirjafferali@gmail.com>
2016-08-06 22:44:38 -0500

    Update install_fencing.sh

--- install_fencing.sh
+++ install_fencing.sh
@@ -8,4 +8,5 @@ HOSTS_ONLINE=$(pcs status | egrep ^Online | awk '{print$3","$4}')
 if [[ -z $(pcs stonith | grep ec2) ]]
 then
   pcs stonith create ec2-fencing fence_ec2 pcmk_host_check="static-list" pcmk_host_list="$HOSTS_ONLINE" tag="Name" action="off" op monitor interval="600s" timeout="300s" op start start-delay="60s" interval="0"
+  pcs property set stonith-enabled=true
 fi  
